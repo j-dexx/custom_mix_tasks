8c58d8cccb549687bd381fe899852851b7cabd3a
James Doyley <jdoyley@gmail.com>
2017-04-11 08:51:41 +0100

    First commit

---
+++ lib/custom_mix.ex
@@ -0,0 +1,18 @@
+defmodule CustomMix do
+  @moduledoc """
+  Documentation for CustomMix.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CustomMix.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
